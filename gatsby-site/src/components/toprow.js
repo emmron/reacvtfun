141e90fc5e7d9b1b7befe713e95e11399bcc5893
unknown <emmetthoolahan@gmail.com>
2019-09-29 20:34:13 +0800

    yeah
    Signed-off-by: unknown <emmetthoolahan@gmail.com>

---
+++ gatsby-site/src/components/toprow.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+
+function Cta(props) {
+    return (
+<div id="card">
+
+</div>
+
+    );
+}
+
+function Avatar(props) {
+    return (
+    <div class="top-row">   
+  <Cta></Cta>  
+<p>Lmao</p>
+    </div>  
+      
+      
+    );
+
+    }
+
+  export default Avatar
---
+++ gatsby-site/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import Avatar from "../components/toprow"
+const IndexPage = () => (
+ <Layout>
+  
+<Avatar/>
+
+</Layout>
+)
+
+export default IndexPage
